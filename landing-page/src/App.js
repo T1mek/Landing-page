a9ced5d3bf9f989785e2ed5b69283b8f80d7de9b
Михаил <misha1234567890m@gmail.com>
2022-07-08 18:29:17 +0300

    Верстка Landing

---
+++ landing-page/src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <div className="header">
+          <img src="/img/logo.png" alt="Logo" />
+          <ul className="menu">
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>SERVICES</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+        <div className="center">
+        <h1>Lorem <span>ipsum</span>, dolor sit amet</h1>
+        <div className="name">
+        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
+        </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
