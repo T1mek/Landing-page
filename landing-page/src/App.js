9a03aa1d876847e74dc9c340d91219c9ecbc1419
T1mek <misha1234567890m@gmail.com>
2022-07-08 23:14:41 +0300

    Добавление стилей

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -12,13 +12,19 @@ function App() {
           </ul>
         </div>
         <div className="center">
-        <h1>Lorem <span>ipsum</span>, dolor sit amet</h1>
-        <div className="name">
-        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
-        </div>
+          <h1>
+            Lorem <span>ipsum</span>, dolor sit amet
+          </h1>
 
-
+          <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
+          <div className="footer">
+          <button className="b1" ><span>Apply Now</span> </button>
+          <button className="b2" >Sign up</button>
+          {/* <img src="/img/main.png" alt="img" /> */}
         </div>
+        </div>
+        
+        
       </div>
     </div>
   );
