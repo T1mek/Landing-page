ac577244235eea226e685a5d2d393637eba3c81d
Михаил <misha1234567890m@gmail.com>
2022-07-11 12:23:06 +0300

    Закончил верстку landing-page

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -12,19 +12,24 @@ function App() {
           </ul>
         </div>
         <div className="center">
+        <div className="z">
           <h1>
             Lorem <span>ipsum</span>, dolor sit amet
           </h1>
 
           <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
           <div className="footer">
-          <button className="b1" ><span>Apply Now</span> </button>
-          <button className="b2" >Sign up</button>
-          {/* <img src="/img/main.png" alt="img" /> */}
+            <button className="b1">
+              <span>Apply Now</span>
+            </button>
+            <button className="b2"><b>Sign up</b></button>
+            {/* <img src="/img/main.png" alt="img" /> */}
+            </div>
+          </div>
+          <div className="k">
+            <img src="/img/main.png" alt="main" />
+          </div>
         </div>
-        </div>
-        
-        
       </div>
     </div>
   );
